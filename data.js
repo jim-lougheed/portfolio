010b56fc77fe8fe3ddc3375a6c1fe8bd3c1c7c1b
Jim Lougheed <jims.codes@gmail.com>
2022-01-27 16:13:22 -0500

    Append Interests section from data

---
+++ data.js
@@ -0,0 +1,39 @@
+const interestsData = [
+  {
+    image: "images/images/languages.jpg",
+    altText: "languages",
+    title: "Language",
+    description:
+      "I'm obsessed with the vast number of ways we can use sounds and symbols to convey complex ideas to one another, and often without even thinking about it. Nothing excites me more than examining new words for familiar concepts, new scripts containing familiar thoughts, or new slang to replace standard messages.",
+  },
+  {
+    image: "images/images/code.jpeg",
+    altText: "web development",
+    title: "Web Development",
+    description:
+      "Last year, a couple good friends introduced me to the world of web development. Although my previous roles were challenging in their own ways, I wanted to take on a new challenge and learn to create applications with my own two hands. Having learned a lot about the way different human languages work, learning computer languages to start coding seemed like a natural next step. With each day, I learn more, build more and grow more curious about the endless possibilities of web development.",
+  },
+  {
+    image: "images/images/cake.jpg",
+    altText: "baking",
+    title: "Baking",
+    description:
+      "It's no secret I have a sweettooth, nor is it that it is spoiled with desserts and goodies. Due in part to the Great Canadian Baking Show, I have sank my teeth into the world of creating elegant and sugary desserts (and then enjoy sinking my teeth into the desserts themselves!). I've come a long way, but I have a lot to learn and bake before I earn the title of Star Baker.",
+  },
+  {
+    image: "images/images/piano.jpg",
+    altText: "piano",
+    title: "Piano",
+    description:
+      "Sitting down at the bench and transforming scribblings into melodies transports me to another world. Since I was a wee lad, I have been in love with the feeling of music emanating from my fingers. It's a beautiful thing.",
+  },
+  {
+    image: "images/images/video-games.jpg",
+    altText: "video games",
+    title: "Video Games",
+    description:
+      "From the moment I received a Nintendo 64 one Christmas, I've had a passion for gaming. A good game brings you into its constructed world and lets you explore and solve challenges within its constructs, and it's exactly this feeling of immersion that I keep coming back for.",
+  },
+];
+
+export default interestsData;
--- index.js
+++ index.js
@@ -1,5 +1,127 @@
+import interestsData from "./data.js";
+
 document.querySelector(".navbar-toggler").addEventListener("click", () => {
   console.log("test");
   document.querySelector(".navbar").style.flexDirection = "column";
   document.getElementById("navbar-nav").style.display = "flex";
+});
+
+const carouselInner = document.querySelector(".carousel-inner");
+
+const displayInterestsData = (data) => {
+  data.map((interest, i) => {
+    const { image, altText, title, description } = interest;
+
+    const carouselItem = createCarouselItem(i);
+    const interestImage = createInterestImage(image, altText);
+    const interestText = createInterestText(i);
+    const interestItem = createInterestItem(i);
+
+    interestText.appendChild(createInterestTitle(title));
+    interestText.appendChild(createInterestDescription(description));
+
+    interestItem.appendChild(interestImage);
+    interestItem.appendChild(interestText);
+
+    carouselItem.appendChild(interestItem);
+
+    carouselInner.appendChild(carouselItem);
+  });
+};
+
+const createCarouselItem = (i) => {
+  const carouselItem = document.createElement("div");
+  carouselItem.classList.add("carousel-item");
+  i === 0 && carouselItem.classList.add("active");
+  return carouselItem;
+};
+
+const createInterestItem = (i) => {
+  const interestItem = document.createElement("div");
+  interestItem.classList.add("interest__item");
+  i % 2 === 0
+    ? interestItem.classList.add("interest__item--even")
+    : interestItem.classList.add("interest__item--odd");
+  return interestItem;
+};
+
+const createInterestImage = (image, altText) => {
+  const interestImage = document.createElement("img");
+  interestImage.classList.add("interest__image");
+  interestImage.setAttribute("src", image);
+  interestImage.setAttribute("alt", altText);
+  return interestImage;
+};
+
+const createInterestText = (i) => {
+  const interestText = document.createElement("div");
+  interestText.classList.add("carousel-caption");
+  interestText.classList.add("d-md-block");
+  interestText.classList.add("interest__text");
+  i % 2 === 0
+    ? interestText.classList.add("interest__text--even")
+    : interestText.classList.add("interest__text--odd");
+  return interestText;
+};
+
+const createInterestTitle = (title) => {
+  const interestTitle = document.createElement("h3");
+  interestTitle.classList.add("interest__title");
+  interestTitle.textContent = title;
+  return interestTitle;
+};
+
+const createInterestDescription = (description) => {
+  const interestDescription = document.createElement("p");
+  interestDescription.classList.add("interest__description");
+  interestDescription.textContent = description;
+  return interestDescription;
+};
+
+displayInterestsData(interestsData);
+
+(function ($) {
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function (partial) {
+    var $t = $(this),
+      $w = $(window),
+      viewTop = $w.scrollTop(),
+      viewBottom = viewTop + $w.height(),
+      _top = $t.offset().top,
+      _bottom = _top + $t.height(),
+      compareTop = partial === true ? _bottom : _top,
+      compareBottom = partial === true ? _top : _bottom;
+
+    return compareBottom <= viewBottom && compareTop >= viewTop;
+  };
+})(jQuery);
+
+var win = $(window);
+
+var allMods = $(".module");
+
+allMods.each(function (i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible");
+  }
+});
+
+win.scroll(function (event) {
+  allMods.each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in");
+    }
+  });
 });
