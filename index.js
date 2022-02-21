681b56e4deecc94cd46d01a1099e249408301f05
Jim Lougheed <jims.codes@gmail.com>
2022-02-21 14:00:22 -0500

    Add profile pic and clean up JS file

--- index.js
+++ index.js
@@ -1,55 +1,60 @@
 import interestsData from "./data.js";
 
-const activeSection = document.querySelector(".section--active");
+// Entry page animation
 
-$(".entry__button").click(() => {
+const delay = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const contentFade = () => {
   $(".entry__page").css("background-color", "transparent");
   $(".entry__container").css("opacity", "0");
-  setTimeout(() => {
-    $(".entry__shape--1").css("width", "0");
-    $(".entry__shape--2").css("width", "0");
-  }, 1000);
-  setTimeout(() => {
-    $(".entry__page").css("opacity", "0");
-  }, 3000);
-  setTimeout(() => {
-    $(".entry__page").remove();
-  }, 4000);
-});
+};
+const splitAnimation = () => {
+  $(".entry__shape--1").css("width", "0");
+  $(".entry__shape--2").css("width", "0");
+};
+
+const handleEnterClick = async () => {
+  contentFade();
+  await delay(1000);
+  splitAnimation();
+  await delay(1000);
+  await $(".entry__page").css("opacity", "0");
+  await delay(1000);
+  await $(".entry__page").remove();
+};
 
 // Tab menu
-const sections = document.querySelectorAll(".section");
-const animationContainer = document.querySelector(".animation-container");
+
+const sections = $(".section").toArray();
+
 const goToSection = (selectedSection) => {
   sections.forEach((section, i) => {
-    // console.log(section.getBoundingClientRect());
-    section.style.left = `${(i - selectedSection) * 100}%`;
-    section.classList.remove("section--active");
+    $(section).css("left", `${(i - selectedSection) * 100}%`);
+    $(section).removeClass("section--active");
     if (Number(selectedSection) === i) {
-      section.classList.add("section--active");
+      $(section).addClass("section--active");
       const maxH = section.getBoundingClientRect().height;
-      console.log(`${maxH}px`, animationContainer);
-      animationContainer.style.height = `${maxH}px`;
-      console.log(animationContainer.style);
+      $(".animation-container").css("height", `${maxH}px`);
     }
   });
 };
 
 $(".sections-nav").click((e) => {
-  if (e.target.classList.contains("sections-nav__link")) {
+  if ($(e.target).hasClass("sections-nav__link")) {
     const { section } = e.target.dataset;
     goToSection(section);
   }
 });
-goToSection(0);
 
 // Project details
-const detailSections = document.querySelectorAll(".project__specs");
-console.log(detailSections);
+
+const detailSections = $(".project__specs").toArray();
 
 detailSections.forEach((section) => {
-  section.addEventListener("click", (e) => {
-    if (e.target.classList.contains("project__spec")) {
+  $(section).click((e) => {
+    if ($(e.target).hasClass("project__spec")) {
       changeSpec(e);
     }
   });
@@ -57,51 +62,61 @@ detailSections.forEach((section) => {
 
 const changeSpec = (e) => {
   const { spec } = e.target.dataset;
-  const allSpecs = Array.from(e.target.parentElement.children);
-  const allDetails = Array.from(
-    e.target.parentElement.nextElementSibling.children
-  );
+  const { parentElement } = e.target;
+  const allSpecs = Array.from(parentElement.children);
+  const allDetails = Array.from(parentElement.nextElementSibling.children);
   changeClass(allSpecs, "project__spec", Number(spec));
   changeClass(allDetails, "project__detail", Number(spec));
 };
 
 const changeClass = (elements, className, spec) => {
   elements.forEach((element, i) => {
-    element.classList.remove(`${className}--active`);
+    $(element).removeClass(`${className}--active`);
     if (i === spec) {
-      element.classList.add(`${className}--active`);
+      $(element).addClass(`${className}--active`);
     }
   });
 };
 
 // Interest details
-const moreInfoInterests = document.querySelectorAll(".interest__spec");
-console.log(moreInfoInterests);
+
+const moreInfoInterests = $(".interest__spec").toArray();
 
 moreInfoInterests.forEach((button) => {
-  button.addEventListener("click", (e) => {
+  $(button).click((e) => {
+    const interestsContainer =
+      e.target.parentElement.parentElement.parentElement.parentElement;
     displayMore(e);
-    animationContainer.style.height = `${
-      e.target.parentElement.parentElement.parentElement.parentElement.getBoundingClientRect()
-        .height + 200
-    }px`;
-    console.log(
-      e.target.parentElement.parentElement.parentElement.parentElement.getBoundingClientRect()
-        .height
+    $(".animation-container").css(
+      "height",
+      `${interestsContainer.getBoundingClientRect().height + 200}px`
     );
   });
 });
 
 const displayMore = (e) => {
   let spec;
-  if (e.target.classList.contains("interest__spec-arrow")) {
+  if ($(e.target).hasClass("interest__spec-arrow")) {
     spec = e.target.parentElement;
   } else {
     spec = e.target;
   }
   $(spec).toggleClass("interest__spec--active");
-  spec.classList.contains("interest__spec--active")
+  $(spec).hasClass("interest__spec--active")
     ? (spec.firstElementChild.innerText = "↑")
     : (spec.firstElementChild.innerText = "↓");
   $(spec.nextElementSibling).toggleClass("interest__detail--active");
 };
+
+// Initialization
+
+const init = () => {
+  $(".entry__button").click(() => {
+    handleEnterClick();
+  });
+  goToSection(0);
+};
+
+$(document).ready(() => {
+  init();
+});
